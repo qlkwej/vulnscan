eb3d3b5efe0cb42428e07553312f264673a08fc6
Charles Green <1191302+charlesgreen@users.noreply.github.com>
2019-05-01 20:03:01 +0900

    Add cli dependency

---
+++ security-bench.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"os"
+	"sort"
+
+	"gopkg.in/urfave/cli.v1"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "lang, l",
+			Value: "english",
+			Usage: "Language for the greeting",
+		},
+		cli.StringFlag{
+			Name:  "config, c",
+			Usage: "Load configuration from `FILE`",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "complete",
+			Aliases: []string{"c"},
+			Usage:   "complete a task on the list",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "add a task to the list",
+			Action: func(c *cli.Context) error {
+				return nil
+			},
+		},
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
