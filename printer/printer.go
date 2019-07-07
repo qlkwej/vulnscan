a6e7f7856d3cd69a5f8f0577d0e858fdbd59beed
Charles Green <1191302+charlesgreen@users.noreply.github.com>
2019-07-07 13:41:45 +0900

    Update linter warnings

--- printer/printer.go
+++ printer/printer.go
@@ -2,15 +2,21 @@ package printer
 
 import "io"
 
+// FormatMethod identifies if the results will be in Log for JSON format
 type FormatMethod int
 
+// AnalysisResult is the results from the security scan
 type AnalysisResult = map[string]interface{}
 
 const (
+	// Store is the apple store lookup
 	Store FormatMethod = iota
+
+	// PList is the plist scan
 	PList
 )
 
+// Printer is the interface used to decouple scans from printing
 type Printer interface {
 	Log(*AnalysisResult, error, FormatMethod)
 	Generate(*io.Writer) error
